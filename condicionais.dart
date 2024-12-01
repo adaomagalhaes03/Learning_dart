316eb99fdd17c83b25a4b06422d54db31a2b3963
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2024-12-01 21:33:54 +0100

    up

--- condicionais.dart
+++ condicionais.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 void main(){
+
+  
    int contar =11;
     //ciclo de repetição mais basico que existe 
    for (var i = 0; i < contar; i++) {
@@ -7,4 +10,17 @@ void main(){
 
    print('Blz, mano, terminou, você é o cara do loop!');
 
+
+  print('digite uma cena mano:');
+  //leitura de dados
+  String? texto = stdin.readLineSync();
+
+  print('Digita um número mano: ');
+  dynamic n1 = stdin.readLineSync();
+
+  print('To say: $texto');
+  
+  print('número: $n1');
+
+  
 }
