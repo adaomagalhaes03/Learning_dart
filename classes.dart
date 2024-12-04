01e1073df7af5910fcb6577859a2c8d15a14f123
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2024-12-04 11:50:58 +0100

    feat: finally course

--- classes.dart
+++ classes.dart
@@ -3,22 +3,36 @@ import 'dart:io';
 class Empresas{
   late String nome;
   late String Sede;
+  //variavel protegida _ não pode ser acessada, está private
+
   late int id;
+  
+  // criando um constutor da vida real mano
+  Empresas(String Nome, String sede, int Id )
+  {
+    this.nome = Nome;
+    this.Sede = sede;
+    this.id = Id;
 
+  }
+ //funcões basicas
 
+ Evento(){
+  print('Novo evento na $nome disponivel!');
+ }
 }
 
 void main(){
+   Empresas TechCompany = new Empresas('Africel','Benguela',1);
+   Empresas Consultoria = new Empresas('Angola Cables','Vila de Viana', 2);
+   print('Empresas cadastradas');
+   print(Consultoria.nome);
+   print(TechCompany.nome);
 
-Empresas unitel = new Empresas();
-unitel.id = 1;
-unitel.nome = 'Unitel';
-unitel.Sede = 'Luanda';
+   //envocando funções que está na class
+   TechCompany.Evento();
+   Consultoria.Evento();
 
+ 
 
-print("------------------------------");
-print("------------EMPRESAS----------");
-print(unitel.id);
-print(unitel.nome);
-print(unitel.Sede);
 }
